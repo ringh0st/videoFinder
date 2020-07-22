2270981b5e44f53251eb03173731dce322fac7d4
Rinat Nadav <5136880-ringhost@users.noreply.gitlab.com>
2020-07-23 02:52:39 +0300

    added a videoSrc and iframe to show vids

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -4,8 +4,12 @@ const VideoDetails =({selectedVideo})=>{
     if(!selectedVideo){
         return <div>Loading...</div>
     }
+    const videoSrc = `https://www.youtube.com/embed/${selectedVideo.id.videoId}`
     return (
     <div>
+        <div className="ui embed">
+            <iframe src={videoSrc}/>
+        </div>
         <div className="ui segment">
             <h4 className="ui header">{selectedVideo.snippet.title}</h4>
             <p>{selectedVideo.snippet.description}</p>
