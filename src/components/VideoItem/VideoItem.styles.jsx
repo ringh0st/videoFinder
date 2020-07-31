2b9640b25f91cbde09ed49446db93823c65136e8
Rinat Nadav <ringhost@gmail.com>
2020-07-31 16:56:03 +0300

    deploy via vercel

--- src/components/VideoItem/VideoItem.styles.jsx
+++ src/components/VideoItem/VideoItem.styles.jsx
@@ -4,7 +4,6 @@ export const VideoContainer = styled.div`
 display:flex !important;
 align-items:center !important;
 cursor:pointer;
-
 `
 export const VideoImage = styled.img`
 max-width:180px !important;
